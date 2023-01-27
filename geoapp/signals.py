11cd1c0dfb9a29784f87374623e83e307c1302dc
Farkhatzhan Makhmudov <faralost@gmail.com>
2023-01-27 23:47:37 +0600

    fix signals

--- geoapp/signals.py
+++ geoapp/signals.py
@@ -7,6 +7,8 @@ from geoapp.models import User, Farmer
 
 @receiver(post_save, sender=User)
 def update_farmer_signal(sender, instance, created, **kwargs):
+    if kwargs['raw']:
+        return
     if created:
         Farmer.objects.create(user=instance)
     instance.farmer.save()
